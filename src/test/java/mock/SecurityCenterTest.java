bd3f179b816b29c67fe7788ec3fad1e4c33a4e09
Ze Dang <4649tz@gmail.com>
2020-08-04 13:07:37 +0800

    [zedang] test: implement SecurityCenterTest via Mockito

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -1,19 +1,35 @@
 package mock;
 
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class SecurityCenterTest {
+
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
 
+    @Mock
+    DoorPanel mockDoorPanel;
+    @InjectMocks
+    SecurityCenter sc;
+
+
     @BeforeEach
     public void setUp() {
+
     }
 
     @Test
     public void shouldVerifyDoorIsClosed() {
+        sc.switchOn();
+        verify(mockDoorPanel).close();
     }
 }
